313b5af381b47c032db1035f84330fbf0a3fc1d7
nicolasdev97 <nicolas.po.im@gmail.com>
2025-04-16 16:27:38 -0500

    Hook de useState en React.

--- bases-react/src/estado-componente-controladores-eventos/Main.js
+++ bases-react/src/estado-componente-controladores-eventos/Main.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Hello = ({ name, age }) => {
   const bornYear = () => new Date().getFullYear() - age;
 
@@ -15,11 +17,18 @@ const Main = () => {
   const name = "Peter";
   const age = 10;
 
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => setCounter(counter + 1), 1000);
+
+  console.log("rendering...", counter);
+
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
+      <div>{counter}</div>
     </div>
   );
 };
