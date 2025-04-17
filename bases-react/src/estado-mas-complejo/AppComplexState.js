8f6f8664f4eb21f87084dc1cb58f8a43956befcf
nicolasdev97 <nicolas.po.im@gmail.com>
2025-04-17 17:09:50 -0500

    Manejando estado complejo de forma más eficiente en React.

--- bases-react/src/estado-mas-complejo/AppComplexState.js
+++ bases-react/src/estado-mas-complejo/AppComplexState.js
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import History from "./History";
+import Button from "./Button";
 
 const AppComplexState = () => {
-  //   const [left, setLeft] = useState(0);
-  //   const [right, setRight] = useState(0);
-  const [clicks, setClisks] = useState({
-    right: 0,
-    left: 0,
-  });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
+
+  //   const [clicks, setClisks] = useState({
+  //     right: 0,
+  //     left: 0,
+  //   });
 
   const handleLeftClicks = () => {
     //Creando un objeto
@@ -17,9 +21,12 @@ const AppComplexState = () => {
     // };
 
     //Más simple
-    const newClicks = { ...clicks, left: clicks.left + 1 };
+    // const newClicks = { ...clicks, left: clicks.left + 1 };
 
-    setClisks(newClicks);
+    // setClisks(newClicks);
+
+    setAllClicks(allClicks.concat("L"));
+    setLeft(left + 1);
   };
 
   const handleRightClicks = () => {
@@ -31,18 +38,24 @@ const AppComplexState = () => {
     // };
 
     //Más simple
-    const newClicks = { ...clicks, right: clicks.right + 1 };
+    // const newClicks = { ...clicks, right: clicks.right + 1 };
 
-    setClisks(newClicks);
+    // setClisks(newClicks);
+
+    setAllClicks(allClicks.concat("R"));
+    setRight(right + 1);
   };
 
   return (
     <div>
       <h1>AppComplexState</h1>
-      {clicks.left}
-      <button onClick={handleLeftClicks}>Left</button>
-      <button onClick={handleRightClicks}>Right</button>
-      {clicks.right}
+      {left}
+      <Button handleClick={handleLeftClicks} text="Left" />
+      <Button handleClick={handleRightClicks} text="Right" />
+      {right}
+      <p>{allClicks.join(" ")}</p>
+      <h2>History:</h2>
+      <History allClicks={allClicks} />
     </div>
   );
 };
