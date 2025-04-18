38a51b3a45eb2c33d475c5d39a6a388ba1467074
nicolasdev97 <nicolas.po.im@gmail.com>
2025-04-18 16:08:50 -0500

    Funcion que devuelve funcion con props personalizadas de una manera más compacta en React.

--- bases-react/src/funcion-devuelve-funcion/Function.js
+++ bases-react/src/funcion-devuelve-funcion/Function.js
@@ -1,18 +1,25 @@
 const Function = () => {
+  //De manera más compleja
   const hello = (who) => {
     const handler = () => {
       console.log("Hello", who);
     };
     return handler;
   };
+
+  //De manera más simple
+  const bye = (who) => () => console.log("Bye", who);
+
   return (
     <div>
       <h2>Funciones que devuelven funciones:</h2>
-      <button onClick={hello("world")}>Imprime "world"</button>
+      <button onClick={hello("world")}>Hello "world"</button>
+      <button onClick={hello("react")}>Hello "react"</button>
+      <button onClick={hello("function")}>Hello "function"</button>
       <br />
-      <button onClick={hello("react")}>Imprime "react"</button>
-      <br />
-      <button onClick={hello("function")}>Imprime "function"</button>
+      <button onClick={bye("world")}>Bye "world"</button>
+      <button onClick={bye("react")}>Bye "react"</button>
+      <button onClick={bye("function")}>Bye "function"</button>
     </div>
   );
 };
